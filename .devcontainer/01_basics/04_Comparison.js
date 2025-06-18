1dfe525f8336955a830c058817bf5902e8c3b52f
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-18 17:36:37 +0000

    changes in file

--- .devcontainer/01_basics/04_Comparison.js
+++ .devcontainer/01_basics/04_Comparison.js
@@ -1,3 +1,20 @@
 console.log("2" > 1);
 console.log("02" > 1);
 console.log(null >= 0);
+
+const isloggedin=false
+const outsideTemp=null
+let useremail;
+ const id=Symbol('123')
+ const anotherid=Symbol('123')
+ console.log(id === anotherid);
+
+ const heros=["shaktiman","naagraj", "doga"];
+ let myobj={
+    name:"rupali",
+    age:22,
+ }
+
+ const myfunction=function(){
+    console.log("hello world");
+ }
