258dfdc136287d3f3b9832c510e3a370613f7084
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-18 10:22:57 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,20 @@
+let score =33
+console.log(typeof score)
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+
+//"33"=> 33
+//'33adbc"=> NaN(not a number)
+//true=> 1; false=>0
+
+let isloggedin=""
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//1=>true; 0=>false
+//""=>false
+//"rupali"=>true
+
+let somenumber= 33
+let stringno=String(somenumber)
+console.log(typeof stringno);
