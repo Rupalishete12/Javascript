b35ce7d4a36b9ddd5aa4506b5f370481d4499651
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-18 10:07:19 +0000

    container config done

--- .devcontainer/01_basics/01_variable.js
+++ .devcontainer/01_basics/01_variable.js
@@ -1,4 +1,11 @@
 const accountId=14433
 let accountemail="rupali12@gmail.com"
 var accountcity="rahuri"
+accountpass="12345"
+let accountstage;
+/*
+Prefer not use var because
+of issue in block scope and functional scope */
+
 console.log(accountId);
+console.table([accountId,accountemail,accountpass,accountcity,accountstage])
---
+++ .devcontainer/01_basics/02_Datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+// treat all js code as newer version
+//alert(3 + 3)    //we are using node.js,not browser
+console.log(3 + 3)
+console.log("rupali")
+let name="rupali"
+let age=23
+let isloggedin= false
+
+console.log(typeof null);  //o/p object
+console.log(typeof undefined);  // undefined
