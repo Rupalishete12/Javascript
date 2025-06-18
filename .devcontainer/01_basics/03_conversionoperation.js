17cd88bd0bf31e5104fe42ccf8cdbd251175116f
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-18 10:42:45 +0000

    conversion and operation done

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -18,3 +18,32 @@ console.log(booleanisloggedin);
 let somenumber= 33
 let stringno=String(somenumber)
 console.log(typeof stringno);
+
+/****** Operations */
+/* let value =3
+let negvalue= -value
+console.log(negvalue);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+console.log(2/2);
+console.log(2/2);*/
+/*
+let str1 ="hello"
+let str2=" rupali"
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+
+console.log("1" + 2 +2);
+console.log(1 +2 + "2");
+
+console.log(3 + 4 * 5 % 3);
+*/
+console.log(+true);
+let num1,num2,num3
+let gamecounter =100;
+gamecounter++;
+console.log(gamecounter);
