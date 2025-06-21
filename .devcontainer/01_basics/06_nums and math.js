e6a9d91123694ac44d5ad0042b9977cdd50672a8
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-21 19:09:01 +0530

    Maths liabrary done

--- .devcontainer/01_basics/06_nums and math.js
+++ .devcontainer/01_basics/06_nums and math.js
@@ -1,4 +1,22 @@
 const score=400
 const balance=new Number(100)
-console.log(balance);
-console.log(balance.toString());
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherno=23.888
+//console.log(otherno.toPrecision(3));
+
+const hundreads=10000
+//console.log(hundreads.toLocaleString('en-IN'));
+
+/***MATHS LIABRARY **/
+
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.round(4,6));
+console.log(Math.random()*10);
+
+const min=10
+const max=10
+console.log(Math.floor(Math.random() * (max -min +1)) +min)
