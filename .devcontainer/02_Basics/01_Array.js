fa1f61b3e9e4f67d7ff695c9ca10fc53d87db0c9
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-21 23:02:02 +0530

    Array

---
+++ .devcontainer/02_Basics/01_Array.js
@@ -0,0 +1,19 @@
+//collection multiple item on same array
+//js array retype
+//0 base indexing
+//it can be create shallow copy // if changes in file it can be change in original file
+//deep copy does not share the same reference
+//methods
+
+//declare array      
+const array1=[0,1,2,3,4]
+
+const array2=new Array(0,1,2,3)      //declare array
+console.log(array1[0]);
+
+//Array methods
+array1.push(6)  //insert
+console.log(array1);
+  
+array1.pop()     //delete
+console.log(array1)
