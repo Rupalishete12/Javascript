01a0cf40d9a2734fca98579a40aa6995c8f2ba78
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-22 14:09:35 +0000

    fuctions done

---
+++ .devcontainer/03_Basics/01_Functions.js
@@ -0,0 +1,21 @@
+
+function saymyname(){
+    console.log("Rupali");
+    console.log("shete");
+
+}
+saymyname()
+
+function addtwono(num1,num2){
+    let result=num1+num2
+    return result
+    console.log("rup");
+  //console.log(num1+num2);
+
+}
+addtwono(3,"4")
+
+function loginuser(username){
+    return '${username} just logged in'
+}
+console.log(loginuser("rupali"))
