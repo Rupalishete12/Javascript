c5d9740dd31a4d904caef0a82a824c8c2676f93e
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-25 23:52:45 +0530

    Add files via upload

---
+++ .devcontainer/03_Basics/04_Dom/script.js
@@ -0,0 +1,23 @@
+//console.dir(document.body);
+//console.log(document.body.style.background="green");
+//console.log(document.body.childNodes[3].innerText="abcd");
+
+
+//selecting with id
+let heading=document.getElementById("heading");
+console.dir("heading");
+
+//selecying with class
+let headings=document.getElementsByClassName("heading-class");
+console.log("headings")
+
+//selecting with tag
+let para=document.getElementsByName("p")
+console.log("para")
+
+//query selector
+let elements=document.querySelector("p");
+console.dir("elements");
+
+//let allelem=document.querySelectorAll("p")
+//console.log(allelem);
