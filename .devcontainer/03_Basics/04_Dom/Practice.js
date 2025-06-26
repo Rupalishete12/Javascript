3dd3430b62b26e65443cceb78b1fd3d858bc3c83
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-26 19:01:14 +0530

    Add files via upload

---
+++ .devcontainer/03_Basics/04_Dom/Practice.js
@@ -0,0 +1,8 @@
+//let h2=document.querySelector("h2");
+//console.log("h2".innerText);
+//h2.innerText=h2.innerText + " from apna college students";  //conactinate(+)
+
+let divs=document.querySelectorAll(".box");
+divs[0].innerText="new unique value 0";
+divs[1].innerText="new unique value 1";
+divs[2].innerText="new unique value 2";
