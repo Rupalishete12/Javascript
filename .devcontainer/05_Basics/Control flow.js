61f0d848d0c3b484cf4c8d7bd82dbad979aa8406
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-06-27 17:23:20 +0000

    Changes done

---
+++ .devcontainer/05_Basics/Control flow.js
@@ -0,0 +1,17 @@
+//if
+const isuserloggerdin = true
+const temperature=41
+if(temperature < 40){
+    console.log("less than 50");
+    
+}
+else{
+console.log("greater than 50");
+}
+
+const score=200
+if(score > 100){
+    const power="fly"
+    console.log('user power : ${power}');
+}
+console.log('user power: ${power}');
