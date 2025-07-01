f66ed19518ba929e3a4d28970dda361b6d494584
Rupalishete12 <162807919+Rupalishete12@users.noreply.github.com>
2025-07-01 17:37:20 +0000

    Changes done

---
+++ .devcontainer/06_Basics/Object and arrays.js
@@ -0,0 +1,17 @@
+let users=[
+    {
+        id:1,
+        name:"rupali",
+        age:22
+    },
+    {
+    id:2,
+    name:"bebo",
+    age:22
+    }
+
+];
+for(let i=0;i<users.length;i++)
+{
+    console.log("user" +users[i].id + ":" + users[i].name + "is" +users[i].age + "years old");
+}
